75b829209901da80da63078d70acd5e5aab516a7
Uriel Benitez <unbenitez@gmail.com>
2026-07-13 10:52:12 -0300

    feat: se agrega validacion de formato al input de palabra

--- unq-ui-uriel-benitez-trabajo-final/src/App.jsx
+++ unq-ui-uriel-benitez-trabajo-final/src/App.jsx
@@ -4,19 +4,20 @@ import { existeLaPalabra } from "./services/palabras.service.js";
 
 function App() {
   const [error, setError] = useState("");
+  const [palabra, setPalabra] = useState("");
 
   const validarPalabra = async () => {
-    const palabra = document.querySelector("input").value;
     try {
       const existe = await existeLaPalabra(palabra);
       if (!existe) {
         setError(`La palabra "${palabra}" no es válida`);
       } else {
         setError("");
+        setPalabra("");
         console.log(`La palabra "${palabra}" es válida`);
       }
     } catch (err) {
-      setError(err);
+      setError(err.message);
     }
   };
 
@@ -27,7 +28,19 @@ function App() {
   return (
     <div className="main-container">
       <h1>Palabras encadenadas</h1>
-      <input type="text" placeholder="Ingrese una palabra" />
+      <input
+        type="text"
+        value={palabra}
+        onChange={(e) =>
+          setPalabra(e.target.value.replace(/[^A-Za-zÀ-ÿñÑ]/g, ""))
+        }
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            validarPalabra();
+          }
+        }}
+        placeholder="Ingresá una palabra"
+      />
       <button onClick={() => validarPalabra()}>Agregar palabra</button>
     </div>
   );
--- unq-ui-uriel-benitez-trabajo-final/src/services/palabras.service.js
+++ unq-ui-uriel-benitez-trabajo-final/src/services/palabras.service.js
@@ -3,7 +3,7 @@ export const existeLaPalabra = async (palabra) => {
     `https://word-api-hmlg.vercel.app/api/validate?word=${encodeURIComponent(palabra)}`,
   );
   if (!response.ok) {
-    throw new Error("Error al validar la palabra");
+    throw new Error("Error al intentar validar la palabra");
   }
   const { exists } = await response.json();
   return exists;
